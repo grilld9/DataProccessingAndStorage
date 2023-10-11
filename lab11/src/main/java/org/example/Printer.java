5f36c9d8223918e823621dd3d55a8a9656f24279
grilld9 <m.maksimenkov@g.nsu.ru>
2023-10-11 17:18:25 +0700

    Add lab11

---
+++ lab11/src/main/java/org/example/Printer.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.concurrent.Semaphore;
+
+public class Printer extends Thread {
+
+    private final Semaphore parentSemaphore, childSemaphore;
+    Printer(Semaphore parentSemaphore, Semaphore childSemaphore) {
+        this.parentSemaphore = parentSemaphore;
+        this.childSemaphore = childSemaphore;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            try {
+                childSemaphore.acquire();
+                System.out.println("Child thread prints string " + i);
+                parentSemaphore.release();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
